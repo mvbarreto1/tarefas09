8e802946b6b0f43bbdc12a575f3eadf8abf48393
mvbarreto1 <marcusbarreto@outlook.com.br>
2024-08-22 00:14:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+//let novoElemento = document.createElement ('div');
+  //  novoElemento.textContent = ('p');
+    //  document.body.appendChild(novoElemento);
+
+      //----
+
+      let Elementlog = document.getElementById('imagem')
+Elementlog.setAttribute('src','imagem')
